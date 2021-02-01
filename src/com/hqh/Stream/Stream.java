28b8ab24a3503854701047f2f1f9fc1c068ae6f5
qyuh <hoquanghuy0123@gmail.com>
2021-02-01 09:32:07 +0700

    git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/qyuh/JAVACORE.git git push -u origin main

---
+++ src/com/hqh/Stream/Stream.java
@@ -0,0 +1,10 @@
+package com.hqh.Stream;
+
+import java.util.stream.IntStream;
+
+public class Stream {
+    public static void main(String[] args) {
+        IntStream.range(1, 4).forEach(System.out::println);
+        IntStream.of(1, 2, 3, 4).forEach(System.out::println);
+    }
+}
