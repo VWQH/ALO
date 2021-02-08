6fa9135b4f0c8e802839352413d60f9eaa7b44c8
qyuh <hoquanghuy0123@gmail.com>
2021-02-08 17:16:14 +0700

    git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/qyuh/JAVACORE.git git push -u origin main

--- src/com/hqh/AD/StringReverse.java
+++ src/com/hqh/AD/StringReverse.java
@@ -1,5 +1,6 @@
 package com.hqh.AD;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class StringReverse {
@@ -19,13 +20,35 @@ public class StringReverse {
         }
         System.out.println("Yes");*/
 
-        String a = "hel";
+        String a = "hello";
+        String b = "olleh";
+
+
         int[] char_fre = new int[26];
         int a_lenght = a.length();
+        int b_lenght = b.length();
+
+        if(a_lenght != b_lenght)
+
+        a = a.toLowerCase();
+        b = b.toLowerCase();
         for (int i = 0; i < a_lenght; i++) {
             char current = a.charAt(i);
             int index = current - 'a';
             char_fre[index]++;
+            System.out.print(current + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < b_lenght; i++) {
+            char current = b.charAt(i);
+            int index = current - 'a';
+            char_fre[index]--;
+            System.out.print(current + " ");
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if(char_fre[i] != 0)
+                System.out.println("No");
         }
 
     }
