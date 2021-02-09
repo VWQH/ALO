b375fee375bfcb84b9bea5e30a6291ebfeff3e29
qyuh <hoquanghuy0123@gmail.com>
2021-02-09 16:39:22 +0700

    git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/qyuh/JAVACORE.git git push -u origin main

---
+++ src/com/hqh/AD/RegexA.java
@@ -0,0 +1,14 @@
+package com.hqh.AD;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexA {
+    public static void main(String[] args) {
+        String regex = "[a-z]+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher("qwe");
+        boolean match = matcher.matches();
+        System.out.println(match);
+    }
+}
