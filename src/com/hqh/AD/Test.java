7fc7b753d4aaf9c9cbdb5f3601d88bf84ef0c059
qyuh <hoquanghuy0123@gmail.com>
2021-02-23 22:56:31 +0700

    git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/qyuh/JAVACORE.git git push -u origin main

--- src/com/hqh/AD/Test.java
+++ src/com/hqh/AD/Test.java
@@ -2,7 +2,7 @@ package com.hqh.AD;
 
 
 public class Test {
-    public static void main(Stringgg[] args) {
+    public static void main(String[] args) {
         int count = 0;
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
